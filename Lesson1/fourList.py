1a8fc9b7d49ba1c460a71498a0405328f1239870
CaptainLoop <janka.szabolcs@gmail.com>
2022-12-22 17:10:28 +0100

    List and list things

---
+++ Lesson1/fourList.py
@@ -0,0 +1,38 @@
+a = ['Attredies', 'Heresy', 'Matshupi', 0, 1.2]
+
+a[0] = "change"
+a.append("leage of hammer")
+
+print(a)
+
+a.insert(2, 'hereIAm')
+
+#insert
+
+b = a + ['kocsog', 'vaza']
+
+print(b)
+print(a)
+
+print(type(a))
+
+print(a[1])
+print(a[0])
+
+print(a[0:5])
+print(a[-1])
+
+#type
+
+print(type(a[4]))
+print(type([2]))
+
+
+
+del(a[3])
+print(a)
+
+print(a.pop())
+
+print(a)
+
