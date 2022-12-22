245961847b2d9cedb0c302e8b13a4281f919d774
CaptainLoop <janka.szabolcs@gmail.com>
2022-12-22 14:14:11 +0100

    types

--- Lesson1/first.py
+++ Lesson1/first.py
@@ -1,6 +1,14 @@
-a = input()
+#a = input()
 
-b = float(input("Enter first number: "))
-c = float=input("Enter second number: ")
+#b = float(input("Enter first number: "))
+#c = float=input("Enter second number: ")
 
-print(a)  
+#print(a)  
+#print()
+#print(b+c)
+
+x,y=input("Enter two numbers: ").split()
+x=int(x)
+y=int(y)
+print(type(x))
+print(x+y)
