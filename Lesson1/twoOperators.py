5570d83489a340f0d4c7eac3326c01144f5156a2
CaptainLoop <janka.szabolcs@gmail.com>
2022-12-22 14:53:35 +0100

    operators, in, is

--- Lesson1/twoOperators.py
+++ Lesson1/twoOperators.py
@@ -8,3 +8,16 @@ print(a)
 print(2==2)
 print(5<4)
 
+
+x=5
+
+x = x | 3
+
+print(2==2 and 3==3)
+
+kifejezes = "mocskosallat"
+benne = "alla"
+nincsbenne = "mcsk"
+
+print(benne in kifejezes)
+print(nincsbenne in kifejezes)
