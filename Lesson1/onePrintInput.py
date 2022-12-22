353d356c82d4e321ab75d8f281d2b532b09c72ef
CaptainLoop <janka.szabolcs@gmail.com>
2022-12-22 16:54:35 +0100

    stringmanipulation

---
+++ Lesson1/onePrintInput.py
@@ -0,0 +1,14 @@
+#a = input()
+
+#b = float(input("Enter first number: "))
+#c = float=input("Enter second number: ")
+
+#print(a)  
+#print()
+#print(b+c)
+
+x,y=input("Enter two numbers: ").split()
+x=int(x)
+y=int(y)
+print(type(x))
+print(x+y)
--- Lesson1/threeString.py
+++ Lesson1/threeString.py
@@ -4,3 +4,23 @@ b = "Romania"
 print(type(a))
 print(type(b))
 
+print("\n")
+print(b)
+print("\t")
+
+print(a)
+
+print(a[4])
+print(a[0:3])
+print(a[-2])
+print(a[3:])
+
+c = "Here a sentence, god emperor"
+
+print("All hail:", c)
+print("God Emperor: %s" %c)
+
+number = 546.876164
+print(number)
+
+print("%.4f" %number)
